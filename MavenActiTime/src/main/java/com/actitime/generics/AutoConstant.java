fb7df71e650cd0105585a61dafff97d78a886012
mohammad.zeeshan <mohammad.zeeshan@192.168.18.45>
2021-02-15 02:14:58 +0530

    Code pushed

---
+++ MavenActiTime/src/main/java/com/actitime/generics/AutoConstant.java
@@ -0,0 +1,7 @@
+package com.actitime.generics;
+
+public interface AutoConstant {
+	String url="https://demo.actitime.com";
+	String sheetName="EmployeeInformation";
+	String fileName="FrameworkData";
+}
